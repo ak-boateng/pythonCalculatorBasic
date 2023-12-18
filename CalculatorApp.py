999bcb542b00dbc27de06d14bf3d319932cc7dfa
Amesese Boateng <66135444+ak-boateng@users.noreply.github.com>
2023-12-18 04:53:47 +0000

    python code

---
+++ CalculatorApp.py
@@ -0,0 +1,60 @@
+# This is a basic Python Project, A calculator to perform Add, Subtract, Divide and Multiply
+
+# Define functions to perform functions
+def addition(number1, number2):
+    return number1 + number2
+
+
+def subtraction(number1, number2):
+    return number1 - number2
+
+
+def multiplication(number1, number2):
+    return number1 * number2
+
+
+def division(number1, number2):
+    return number1 / number2
+
+
+# Function to get a valid operand from the user
+def get_operand():
+    while True:
+        operand = int(input("Enter Operand 1 - 4: "))
+        if 1 <= operand <= 4:
+            return operand
+        else:
+            print("Invalid input. Please enter a number between 1 and 4.")
+
+
+# Display options to the user
+print("Enter the operation you want to perform:\n" +
+      "1. Addition\n" +
+      "2. Subtraction\n" +
+      "3. Multiplication\n" +
+      "4. Division: ")
+
+# Get a valid operand from the user
+operand = get_operand()
+
+# Ask the user for input
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+
+# Function to call which operation function when needed
+def calculate():
+    if operand == 1:
+        return addition(number1, number2)
+    elif operand == 2:
+        return subtraction(number1, number2)
+    elif operand == 3:
+        return multiplication(number1, number2)
+    elif operand == 4:
+        return division(number1, number2)
+
+
+# Display results back to the user
+result = calculate()
+if result is not None:
+    print(f"The result is: {result}")
